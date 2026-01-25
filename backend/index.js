d0a18f862dc10a6faa907c1d756aa2787ddd3440
Artyom Matveev <matveev.arty@gmail.com>
2026-01-25 15:19:49 +0700

    db models added, get-req tested

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 require('dotenv').config(); // Для считывания файла .env
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors()); // Настройка CORS для возможности отправки запросов с браузера
+app.use(express.json()); // Возможность парсинга JSON-формата для приложения
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'WORKING SUCCESSFULLY' })
+})
 
 const start = async () => {
     try {

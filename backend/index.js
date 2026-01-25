daff5b9d27146709cb2859b233358c307d768772
Artyom Matveev <matveev.arty@gmail.com>
2026-01-25 14:03:41 +0700

    db env setup, db connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 require('dotenv').config(); // Для считывания файла .env
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+    try {
+        // Установка подключения к БД
+        await sequelize.authenticate();
+
+        // Сверка состояния БД со схемой данных БД
+        await sequelize.sync();
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+start();
